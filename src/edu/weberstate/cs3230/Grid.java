2f5709e29cc8656a475226e755b15f9f49f8532e
TomBowker <thomasbowker@mail.weber.edu>
2017-01-23 15:20:53 -0700

    Adding pseudo code to layout plan for my functions and requirements

---
+++ src/edu/weberstate/cs3230/Grid.java
@@ -0,0 +1,16 @@
+package edu.weberstate.cs3230;
+
+/**
+ * Created by Tom on 1/23/2017.
+ */
+public class Grid {
+
+    //create and initialize a 2D array
+    // insert '$' to show that it is filled
+
+    //for testing purposes we need to create a display array
+    //this will show what is being selected and what is not being selected
+
+    //we need to test for what empty and what is not empty
+    //isEmpty()
+}
--- src/edu/weberstate/cs3230/Main.java
+++ src/edu/weberstate/cs3230/Main.java
@@ -3,6 +3,13 @@ package edu.weberstate.cs3230;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+	    // write your code here
+        Grid grid = new Grid();
+
+        //create a while loop that takes input 0-9 and A-J
+        //Test for letters or numbers that are not included in aforementioned list
+        //use the letter Q or q for quiting
+        //give an output if the space has been filled or is empty
     }
 }
