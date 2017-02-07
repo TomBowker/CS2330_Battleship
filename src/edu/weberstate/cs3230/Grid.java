445180121aafc5e849dbcd01dda4552923c89967
TomBowker <thomasbowker@mail.weber.edu>
2017-02-06 17:39:18 -0700

    First commit to master

--- src/edu/weberstate/cs3230/Grid.java
+++ src/edu/weberstate/cs3230/Grid.java
@@ -3,14 +3,58 @@ package edu.weberstate.cs3230;
 /**
  * Created by Tom on 1/23/2017.
  */
+
 public class Grid {
 
     //create and initialize a 2D array
     // insert '$' to show that it is filled
+    private String[][] gameboard = new String[10][10];
+
+    public void Board() {
+
+        //This will build my 2D Array and I will put the symbol $ for testing purposes
+        //$ = empty
+        for(int row = 0; row < gameboard.length; row++){
+            for(int column = 0; column < gameboard.length; column++){
+                gameboard[row][column] = "$";
+            }
+        }
+    }
+
+    //we need the ability to insert data into the array and replace the value
+    public void InsertItem(char row, int column){
+
+        int x = (int)row - 65;
+        int y = column;
+
+        //if the input is proper then replace that spot with a 1
+        gameboard[x][y] = "0";
+    }
+
+    /*These Methods are for testing only*/
 
     //for testing purposes we need to create a display array
     //this will show what is being selected and what is not being selected
+    public void DisplayBoard(){
+
+        //This will help in testing so I can see what is being used and what is not
+        for(int row = 0; row < gameboard.length; row++){
+            for(int column = 0; column < gameboard.length; column++){
+                System.out.print(gameboard[row][column] + " ");
+            }
+            System.out.println();
+        }
+    }
 
     //we need to test for what empty and what is not empty
+    //this will take in an input of a String row and String column
     //isEmpty()
+    public void IsEmpty(char row, int column){
+
+        if(!(gameboard[(int)row - 65][column].equals("0")))
+            System.out.println("location: " + row + ", " + column + " is empty");
+        else
+            System.out.println("location: " + row + ", " + column + " is occupied");
+    }
+
 }
--- src/edu/weberstate/cs3230/Main.java
+++ src/edu/weberstate/cs3230/Main.java
@@ -1,15 +1,68 @@
 package edu.weberstate.cs3230;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
-	    // write your code here
+	    //create an instance of a scanner and my class Grid
         Grid grid = new Grid();
+        Scanner scanner = new Scanner(System.in);
 
-        //create a while loop that takes input 0-9 and A-J
-        //Test for letters or numbers that are not included in aforementioned list
-        //use the letter Q or q for quiting
-        //give an output if the space has been filled or is empty
+        //variables for data collection
+        String input = "";
+        char alphaInput;
+        String numericalInput;
+        int convertedNumInput;
+
+        //create a game board and test whats inside
+        grid.Board();
+        grid.DisplayBoard();
+
+        while(!(input == "Q")){
+
+            //prompt users for the first coordinate
+            System.out.println("Please enter a A-J character:");
+            alphaInput = scanner.next().toUpperCase().charAt(0);
+            System.out.println();
+
+            if(alphaInput == 'Q') {
+                System.out.println("Have a Nice Day");
+                break;
+            } //else if(alphaInput == )
+
+            //prompt users for the second coordinate
+            System.out.println("Please enter 0-9 number:");
+            numericalInput = scanner.next().toUpperCase();
+
+            if(numericalInput.equals("Q")){
+                System.out.println("Have a Nice Day");
+                break;
+            }
+
+            convertedNumInput = Integer.parseInt(numericalInput);
+
+            if(convertedNumInput > 9){
+                System.out.println("INVALID INPUT");
+                break;
+            }
+
+            //insert coordinate into grid
+            grid.InsertItem(alphaInput, convertedNumInput);
+
+            //for testing purposes see if that part of the array has been filled
+            grid.IsEmpty(alphaInput, convertedNumInput);
+
+            System.out.println("Continue.... press enter, or hit Q to quit");
+            input = scanner.next().toUpperCase();
+
+            if(input.equals("Q")){
+                break;
+            }
+        }
+
+        //After input
+        grid.DisplayBoard();
     }
 }
